8f238de42f1984f0c36bf056604a51c34bc57565
FrannyCarabez <37878322+FrannyCarabez@users.noreply.github.com>
2018-05-06 14:20:58 +0200

    jumping cube

---
+++ Infinity/Assets/PlayerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float jumpPower = 10.0f;
+    Rigidbody2D myrigidbody;
+    public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = transform.GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myrigidbody.AddForce(Vector3.up * (jumpPower * myrigidbody.mass * myrigidbody.gravityScale * 20.0f));
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+
+    }
+}
